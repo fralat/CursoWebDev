f2d391988b77c79d6759172282717c50a3dcd9c6
fralat <48969138+fralat@users.noreply.github.com>
2019-03-26 20:14:28 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+echo "<h3>Hello World!</h3>";
+echo "<p><a href='Login.php'>Login</a></p>";
+echo "<a href='Register.php'>Register</a>";
+?>
+</body>
+</html>
